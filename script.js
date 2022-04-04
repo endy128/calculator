668e0080577d8d8158b21ce954beb84791f484f9
Neil Douglas <endy64@icloud.com>
2022-04-04 20:14:59 +0100

    Basics of calculator functions done

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const add = (a, b) => a + b;
+const add = (a, b) => parseInt(a) + parseInt(b);
 
 const subtract = (a, b) => a - b;
 
@@ -6,7 +6,8 @@ const multiply = (a, b) => a * b;
 
 const divide = (a, b) => a / b;
 
-var operator = 'divide';
+var operator = 'add';
+var screenValue = '';
 
 function operate(op, a, b) {
     if (op === 'add') {
@@ -20,4 +21,70 @@ function operate(op, a, b) {
     }
 };
 
-console.log(operate(operator, 2, 3));
+
+function setScreen(value) {
+    document.querySelector(".screen").innerHTML = value;
+    screenValue = value;
+}
+
+
+
+function replyClick(clickedId) {
+    console.log(clickedId);
+    if (clickedId === 'cancel') {
+        clearScreen();
+        return;
+    } else if (clickedId === 'divide'){
+        operator = clickedId;
+        a = screenValue;
+        clearScreen();
+        return;
+    } else if (clickedId === 'multiply'){
+        operator = clickedId;
+        a = screenValue;
+        clearScreen();
+        return;
+    } else if (clickedId === 'subtract'){
+        operator = clickedId;
+        a = screenValue;
+        clearScreen();
+        return;
+    } else if (clickedId === 'add'){
+        operator = clickedId;
+        a = screenValue;
+        clearScreen();
+        return;
+    } else if (clickedId === 'equals'){
+        b = screenValue;
+        getResult(operator, a, b);
+    } else if (clickedId === '0' 
+            || clickedId === '1' 
+            || clickedId === '2' 
+            || clickedId === '3' 
+            || clickedId === '4' 
+            || clickedId === '5' 
+            || clickedId === '6' 
+            || clickedId === '7' 
+            || clickedId === '8' 
+            || clickedId === '9' 
+            || clickedId === '.' ) {
+        if (screenValue.includes('.') && clickedId === '.') return; 
+        screenValue += clickedId;
+        setScreen(screenValue);
+    }
+
+}
+
+function getResult(operator, a, b) {
+        setScreen(operate(operator, a, b));
+}
+
+
+function clearScreen() {
+    setScreen('');
+}
+
+function allClear() {
+    screenValue = '';
+    clearScreen();
+}
